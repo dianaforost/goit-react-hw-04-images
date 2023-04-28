fccdb6a585d9d56bf3c4b1b568c4aff67b45bfea
Diana Forostiana <dianaforost@gmail.com>
2023-04-28 12:15:10 +0200

    task

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,32 +1,27 @@
 import css from '../styles.module.css';
-import React, {Component} from 'react';
+import React, { useState } from 'react';
 import Modal from '../Modal/Modal'
 import propTypes from 'prop-types';
- class ImageGalleryItem extends Component{
-    state ={
-        onModal: false,
-    }
-    onModal =() =>{
-        this.setState(({ onModal }) => ({ onModal: !onModal }));
-    }
-    render(){
-        const { webformatURL, tags, largeImageURL} = this.props;
-    return (
-      <li className={css.ImageGalleryItem}>
-        <img
-          className={css.ImageGalleryItemImage}
-          src={webformatURL}
-          alt={tags}
-          onClick={this.onModal}
-        />
-      {this.state.onModal && <Modal  onClose={this.onModal} largeImageURL={largeImageURL} tag={tags}/>}
-      </li>
-    );
+export default function ImageGalleryItem({webformatURL, tags, largeImageURL}){
+const [onOpenModal, setOnOpenModal] = useState(false)
+const onModal = ()=>{
+  setOnOpenModal(!onOpenModal)
 }
+return (
+  <li className={css.ImageGalleryItem}>
+    <img
+      className={css.ImageGalleryItemImage}
+      src={webformatURL}
+      alt={tags}
+      onClick={onModal}
+    />
+  {onOpenModal && <Modal  onClose={onModal} largeImageURL={largeImageURL} tag={tags}/>}
+  </li>
+);
 }
+
 ImageGalleryItem.propTypes ={
   webformatURL: propTypes.string.isRequired,
   tags: propTypes.string.isRequired,
   largeImageURL: propTypes.string.isRequired
 }
-export default ImageGalleryItem;
